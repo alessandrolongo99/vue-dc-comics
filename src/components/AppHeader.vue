24da7b8729a432abb556c531081253c887b5102b
alessandrolongo99 <longoalessandro153@gmail.com>
2022-07-15 16:28:49 +0200

    add nav-bar

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,77 @@
+<template>
+  <nav class="d-flex justify-content-around align-items-center">
+    <img src="../assets/img/dc-logo.png" alt="dc-logo">
+    <ul class="d-flex align-items-center">
+        <li v-for="(link, index) in navLinks" :key="index" class="text-uppercase fw-bold">
+            {{link.text}}
+        </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+            navLinks:[
+                {
+                text: 'characters',
+                url: '#'
+                },
+                {
+                text: 'comics',
+                url: '#'
+                },
+                {
+                text: 'movies',
+                url: '#'
+                },
+                {
+                text: 'tv',
+                url: '#'
+                },
+                {
+                text: 'games',
+                url: '#'
+                },
+                {
+                text: 'collectibles',
+                url: '#'
+                },
+                {
+                text: 'videos',
+                url: '#'
+                },
+                {
+                text: 'fans',
+                url: '#'
+                },
+                {
+                text: 'news',
+                url: '#'
+                },
+                {
+                text: 'shop',
+                url: '#'
+                },
+            ]
+        }
+    }
+};
+</script>
+
+<style>
+nav{
+    padding: 1rem 0;
+}
+
+img{
+    height: 5rem;
+}
+
+ul li{
+    text-decoration: none;
+    display: inline-block;
+    padding: 1rem;
+}
+</style>
