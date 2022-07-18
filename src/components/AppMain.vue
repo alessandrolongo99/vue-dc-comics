af698b74d55a58358b43dc2f1430f8f13786b79b
alessandrolongo99 <longoalessandro153@gmail.com>
2022-07-18 16:36:05 +0200

    add jumbotron and favicon

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="container-bg-dark">
+    <div class="jumbo">
+      <div class="container position-relative">
+        <div class="floating-text text-white text-uppercase fs-3 fw-bold px-4 py-2 position-absolute">current series</div>
+      </div>
+    </div>
     <div class="container fs-1 text-white">
       <AppProducts/>
     </div>
@@ -24,5 +29,21 @@ export default {
 
 .container {
   padding: 4rem 0;
+}
+
+.jumbo{
+  background-image: url('../assets/img/jumbotron.jpg');
+  background-size: cover;
+  height: 400px;
+}
+
+.jumbo > div{
+  height: 100%;
+}
+
+.floating-text{
+  background-color: #0282f9;
+  bottom: 0;
+  transform: translate(0, 50%);
 }
 </style>
